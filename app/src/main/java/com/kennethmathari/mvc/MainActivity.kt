61ad94e4f75c83fbdacdf0c8f20b81c08e0b5e47
Kenneth Mathari <mathari580@gmail.com>
2022-02-06 18:31:37 +0300

    Implemented Activity Code

--- app/src/main/java/com/kennethmathari/mvc/MainActivity.kt
+++ app/src/main/java/com/kennethmathari/mvc/MainActivity.kt
@@ -2,21 +2,36 @@ package com.kennethmathari.mvc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kennethmathari.mvc.controller.ILoginController
+import com.kennethmathari.mvc.controller.LoginController
 import com.kennethmathari.mvc.databinding.ActivityMainBinding
+import com.kennethmathari.mvc.view.ILoginView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ILoginView {
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var loginController:ILoginController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loginController=LoginController(this)
 
         binding.userButton.setOnClickListener { login() }
     }
 
     private fun login() {
-        TODO("Not yet implemented")
+        loginController.OnLogin(binding.userEmail.text.toString().trim(), binding.userPassword.text.toString().trim())
+    }
+
+    override fun onLoginSuccess(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onLoginError(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
     }
 }
 
