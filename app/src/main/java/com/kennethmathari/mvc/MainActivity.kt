8d66e51c6a3f98bb7b5996edc9cbe367b9e6e1c7
Kenneth Mathari <mathari580@gmail.com>
2022-02-08 15:51:09 +0300

    Added Key Listener

--- app/src/main/java/com/kennethmathari/mvc/MainActivity.kt
+++ app/src/main/java/com/kennethmathari/mvc/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.kennethmathari.mvc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.kennethmathari.mvc.controller.ILoginController
 import com.kennethmathari.mvc.controller.LoginController
 import com.kennethmathari.mvc.databinding.ActivityMainBinding
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity(), ILoginView {
         loginController=LoginController(this)
 
         binding.userButton.setOnClickListener { login() }
+        binding.userEmail.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.userPassword.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun login() {
@@ -32,6 +38,17 @@ class MainActivity : AppCompatActivity(), ILoginView {
 
     override fun onLoginError(message: String) {
         Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
